efc0bdd3db7ba98fa7611f1257e55ab8152434df
DuvManR <roeiduv@gmail.com>
2024-08-26 21:15:39 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,4 @@
-const corsUrl = "https://cors-anywhere.herokuapp.com/corsdemo"
-const corsUrlLink = '<a href="' + corsUrl + '" target="_blank">' + corsUrl + '</a>'
-`אם שגיאה זאת קופצת בעת חישוב הסכם של בן גוריון, יש להיכנס לקישור:<br> ${corsUrlLink} <br> ללחוץ על הכפתור: <br> 'Request temporary access to the demo server' <br>ולנסות שוב.<br> (חלק ממנגנון אבטחתי של אתר האוניברסיטה - כך תתאפשר לכם גישה ל-24 שעות)`
+
 
 
 
