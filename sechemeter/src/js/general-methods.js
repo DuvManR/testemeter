3c06bfe913dd00a095a21eed28f8e9bba20cbb68
DuvManR <roeiduv@gmail.com>
2024-09-07 04:45:33 +0300

    Delete sechemeter directory

--- sechemeter/src/js/general-methods.js
+++
@@ -1,8 +0,0 @@
-// ~~~~~~~~~~~~~~~ \\
-// General Methods \\
-// ~~~~~~~~~~~~~~~ \\
-
-// Gets the SELECT-ELEMENT-ID and returns the selected value
-export const getSelectedOption = function(selectElementId) {
-	return document.getElementById(selectElementId).value;
-}
