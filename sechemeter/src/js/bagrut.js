16c07f5948e7c0def422474f176ccb8d0e24a71f
DuvManR <roeiduv@gmail.com>
2024-09-09 04:36:12 +0300

    Delete sechemeter directory

--- sechemeter/src/js/bagrut.js
+++
@@ -1,5 +0,0 @@
-import bagrutTableBody, {
-	createBagrutRow,
-} from "./bagrut/create-bagrut-table.js";
-import bagrutUniversitySwitch from "./bagrut/update-bagrut-table.js";
-import animation from "./bagrut/handle-mobile-rotation.js";
